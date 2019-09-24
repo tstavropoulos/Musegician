383a2f7c17c367fd882bd7716e556cfde66d3eed
Trevor Stavropoulos <Trevor.Stavropoulos@gmail.com>
2019-09-24 10:34:15 -0700

    Initial partial migration to net core 3.0

--- MusegicianCore/Properties/Settings.Designer.cs
+++ MusegicianCore/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Musegician.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,22 +27,10 @@ namespace Musegician.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"~\\Documents\\Musegician.Museg" +
-            "icianData.mdf\";Integrated Security=True;Connect Timeout=30")]
+            "icianData.mdf\";Integrated Security=False;Connect Timeout=30")]
         public string Musegician_MusegicianDataConnectionString {
             get {
                 return ((string)(this["Musegician_MusegicianDataConnectionString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Trevor Stavropoulos" +
-            "\\Documents\\Musegician.MusegicianData.mdf\";Integrated Security=True;Connect Timeo" +
-            "ut=30")]
-        public string Musegician_MusegicianDataConnectionString1 {
-            get {
-                return ((string)(this["Musegician_MusegicianDataConnectionString1"]));
             }
         }
     }
